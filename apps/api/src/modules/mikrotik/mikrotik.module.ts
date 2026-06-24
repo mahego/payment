2ddeb79abf71fbf021e400d004819855feedd4cd
Mario Antonio Hernandez Gomez <mahegots@gmail.com>
2026-06-24 17:36:45 -0600

    feat: import NetworkActionsModule into MikrotikModule using forwardRef

--- apps/api/src/modules/mikrotik/mikrotik.module.ts
+++ apps/api/src/modules/mikrotik/mikrotik.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { MikrotikService } from './mikrotik.service';
 import { MikrotikController } from './mikrotik.controller';
 import { MikrotikProfilesController } from './mikrotik-profiles.controller';
 import { CutoffCronService } from './cutoff-cron.service';
+import { NetworkActionsModule } from '../network-actions/network-actions.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    forwardRef(() => NetworkActionsModule),
+  ],
   controllers: [MikrotikController, MikrotikProfilesController],
   providers: [MikrotikService, CutoffCronService],
   exports: [MikrotikService, CutoffCronService],
